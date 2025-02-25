16a3cdd70d3455f3c726cc697ff3e7936d42a1d3
Michael Mboya <mikemboya2@gmail.com>
2025-02-25 12:17:15 +0300

    Add the words repository

---
+++ lib/repositories/words_repository.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_login_example/providers/providers.dart';
+
+import '../models/models.dart';
+
+part 'words_repository.g.dart';
+
+class WordsRepository {
+  WordsRepository({required this.supabaseClient});
+
+  final SupabaseClient supabaseClient;
+
+  Stream<Iterable<Word>> getWords() {
+    return supabaseClient
+        .from('words')
+        .stream(primaryKey: ['id'])
+        .eq('is_active', true)
+        .map(
+          (wordsJson) => wordsJson.map(Word.fromJson),
+        );
+  }
+
+  Future<void> addWord({
+    required String word,
+    required String userID,
+  }) async {
+    await supabaseClient.from('words').insert({
+      'word': word,
+      'user_id': userID,
+    });
+  }
+
+  Future<void> deleteWord({required int id}) async {
+    await supabaseClient.from('words').update({
+      'is_active': false,
+      'deleted_at': DateTime.now().toIso8601String(),
+    }).eq('id', id);
+  }
+}
+
+@riverpod
+Future<WordsRepository> wordsRepository(Ref ref) async {
+  final supabase = await ref.watch(supabaseProvider.future);
+  return WordsRepository(supabaseClient: supabase.client);
+}
---
+++ lib/repositories/words_repository.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'words_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$wordsRepositoryHash() => r'59a920d198f26102fe1c1faacd5168cf534292ef';
+
+/// See also [wordsRepository].
+@ProviderFor(wordsRepository)
+final wordsRepositoryProvider =
+    AutoDisposeFutureProvider<WordsRepository>.internal(
+  wordsRepository,
+  name: r'wordsRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$wordsRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef WordsRepositoryRef = AutoDisposeFutureProviderRef<WordsRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
